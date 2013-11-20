e0cce015d6ac3d85a5b733ad8d9821e7acc92fe7
pingvin <r-n-b@bk.ru>
2013-11-20 04:14:59 +0400

    fix bug and validate

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
             var  scroll = $(window).scrollTop();
             var window_height = $(window).height();
             var elem_height = elem.height();
-            console.log(window_height + " " + elem_height);
             elem.css({'position':'absolute','top':'0'});
             start_top = elem.offset().top;
             if((scroll >= start_top)){
@@ -43,4 +42,66 @@ $(document).ready(function() {
     $('.input_group .input_text').focusout(function(){
         $(this).parent().removeClass("input_group_focus");
     });
+
+    //Validate
+    jQuery.validator.addMethod("require_from_group", function(value, element, options) {
+        var numberRequired = options[0];
+        var selector = options[1];
+        var fields = $(selector, element.form);
+        var filled_fields = fields.filter(function() {
+            return $(this).val() != "";
+        });
+        var empty_fields = fields.not(filled_fields);
+        if (filled_fields.length < numberRequired && empty_fields[0] == element) {
+            $(".input_group").addClass("error_group");
+            return false;
+        }
+        $(".input_group").removeClass("error_group");
+        return true;
+    }, jQuery.format("ФИО"));
+
+    jQuery.validator.addMethod("usPhoneFormat", function (value, element) {
+        return this.optional(element) || /^\+7 \d{3} \d{3}\-\d{2}-\d{2} ?$/.test(value);
+    }, "телефон");
+
+    var validator = $(".form_vote").validate({
+        //errorContainer: $(".error_container"),
+        errorLabelContainer: $(".error_container"),
+        wrapper: 'p',
+        focusCleanup: true,
+        rules: {
+            // note: you can use variable to avoid repeating
+            // of the same code
+            first_name: {
+                require_from_group: [3, ".input_group input"]
+            },
+            last_name: {
+                require_from_group: [3,".input_group input"]
+            },
+            second_name: {
+                require_from_group: [3,".input_group input"]
+            },
+            phone: {
+                usPhoneFormat: true,
+                required: true
+            }
+        },
+        showErrors: function(errorMap, errorList) {
+            $(".hide_p").show();
+            this.defaultShowErrors();
+        }
+    });
+    $(".form_vote input,.form_vote textarea").on("keyup", function () {
+        if ($(".input_group .error").length) {
+            $(".input_group").addClass("error_group");
+        }
+        if (validator.form()) {
+            $(".btn_plus").removeClass("btn_plus_disable");
+            $(".button_block").addClass("validate_ok");
+            $(".hide_p").hide();
+        } else {
+            $(".btn_plus").addClass("btn_plus_disable");
+            $(".button_block").removeClass("validate_ok");
+        }
+    })
 });
